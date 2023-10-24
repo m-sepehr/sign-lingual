b9fff8f3abb67fda0b18cdeec9e694f88654c64c
mycoal2 <michaelhong136@gmail.com>
2023-10-24 15:38:08 -0400

    Added Camera and Navigation Drawer

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
@@ -1,14 +1,169 @@
 package com.example.signlingual;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.ImageReader;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askCameraPermission();
+
+        setupUI();
     }
+
+    public void askCameraPermission() {
+        //TODO ask for camera permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.CAMERA}, 121);
+            }else{
+                //TODO show live camera footage
+                setFragment();
+            }
+        } else {
+            //TODO show live camera footage
+            setFragment();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //TODO show live camera footage
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //TODO show live camera footage
+            setFragment();
+        } else {
+            finish();
+        }
+    }
+
+    //TODO fragment which show live footage from camera
+    int previewHeight = 0,previewWidth = 0;
+    int sensorOrientation;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected void setFragment() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+        try {
+            cameraId = manager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        CameraConnectionFragment fragment;
+        CameraConnectionFragment camera2Fragment =
+                CameraConnectionFragment.newInstance(
+                        new CameraConnectionFragment.ConnectionCallback() {
+                            @Override
+                            public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                previewHeight = size.getHeight();
+                                previewWidth = size.getWidth();
+                                Log.d("tryOrientation","rotation: "+rotation+"   orientation: "+getScreenOrientation()+"  "+previewWidth+"   "+previewHeight);
+                                sensorOrientation = rotation - getScreenOrientation();
+                            }
+                        },
+                        this,
+                        R.layout.camera_fragment,
+                        new Size(640, 480));
+
+        camera2Fragment.setCamera(cameraId);
+        fragment = camera2Fragment;
+        getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+    protected int getScreenOrientation() {
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_270:
+                return 270;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_90:
+                return 90;
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public void onImageAvailable(ImageReader imageReader) {
+        imageReader.acquireLatestImage().close();
+    }
+
+    private void setupUI() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("SignLingual");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == android.R.id.home) {
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+//                Toast.makeText(MainActivity.this, "inside", Toast.LENGTH_SHORT).show();
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+//                Toast.makeText(MainActivity.this, "else", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == R.id.navHome) {
+            // Already Home
+        } else if (itemID == R.id.page1) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        } else if (itemID == R.id.page2) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }
