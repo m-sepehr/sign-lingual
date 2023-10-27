4ba82af557d1156571317f0319432c963ffee2e8
mycoal2 <michaelhong136@gmail.com>
2023-10-27 18:36:35 -0400

    Refactored MainActivity

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
@@ -31,7 +31,7 @@ import android.Manifest;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener, NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
@@ -41,86 +41,7 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        askCameraPermission();
-
         setupUI();
-    }
-
-    public void askCameraPermission() {
-        //TODO ask for camera permissions
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ) {
-                ActivityCompat.requestPermissions(this, new String[]{
-                        Manifest.permission.CAMERA}, 121);
-            }else{
-                //TODO show live camera footage
-                setFragment();
-            }
-        } else {
-            //TODO show live camera footage
-            setFragment();
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        //TODO show live camera footage
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            //TODO show live camera footage
-            setFragment();
-        } else {
-            finish();
-        }
-    }
-
-    //TODO fragment which show live footage from camera
-    int previewHeight = 0,previewWidth = 0;
-    int sensorOrientation;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    protected void setFragment() {
-        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        String cameraId = null;
-        try {
-            cameraId = manager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        CameraConnectionFragment fragment;
-        CameraConnectionFragment camera2Fragment =
-                CameraConnectionFragment.newInstance(
-                        new CameraConnectionFragment.ConnectionCallback() {
-                            @Override
-                            public void onPreviewSizeChosen(final Size size, final int rotation) {
-                                previewHeight = size.getHeight();
-                                previewWidth = size.getWidth();
-                                Log.d("tryOrientation","rotation: "+rotation+"   orientation: "+getScreenOrientation()+"  "+previewWidth+"   "+previewHeight);
-                                sensorOrientation = rotation - getScreenOrientation();
-                            }
-                        },
-                        this,
-                        R.layout.camera_fragment,
-                        new Size(640, 480));
-
-        camera2Fragment.setCamera(cameraId);
-        fragment = camera2Fragment;
-        getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-    }
-    protected int getScreenOrientation() {
-        switch (getWindowManager().getDefaultDisplay().getRotation()) {
-            case Surface.ROTATION_270:
-                return 270;
-            case Surface.ROTATION_180:
-                return 180;
-            case Surface.ROTATION_90:
-                return 90;
-            default:
-                return 0;
-        }
-    }
-
-    @Override
-    public void onImageAvailable(ImageReader imageReader) {
-        imageReader.acquireLatestImage().close();
     }
 
     private void setupUI() {
