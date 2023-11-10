c6ef07bb4437d2b1185dcb9a225819e8decec687
m-sepehr <m-sepehr@hotmail.com>
2023-11-09 23:01:20 -0500

    adding ability to make api call to OpenAi's GPT. added ability to capture detected letters and upload to GPT for typo correction. added text to speech to play audio for corrected words. api call and text to speech currently commented out.

--- sign-lingual-ml/main/useModel.py
+++ sign-lingual-ml/main/useModel.py
@@ -1,11 +1,17 @@
+import os
+import openai
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras import backend as K
+import gtts
+from playsound import playsound
+
 
 # constants
-image_width, image_height = 200, 200  
+image_width, image_height = 200, 200 
+#openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def f1_metric(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -51,7 +57,31 @@ def predict_asl_letter(landmarks):
     
     return predicted_label, predicted_prob
 
+# def correct_typo(word, context_buffer):
+    # adding context window for better typo corrections
+    context = ' '.join(context_buffer[-5:])  # window size can be adjusted here
 
+    completion = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=[
+            {"role": "system", "content": "You correct typos in the last word and return only the last word's correction. Don't add punctuation. If you don't understand the last word, just return the original last word. Nothing else."},
+            {"role": "user", "content": context + ' ' + word},
+        ],
+        #temperature=1,
+    )
+    # extracting the last word from the completion as a precaution
+    corrected_word = completion.choices[0].message.content.strip().split()[-1]
+    
+    return corrected_word
+
+# variables to keep track of the current symbol and how many consecutive frames it has appeared in
+current_symbol = None
+previous_symbol = None
+current_symbol_count = 0
+captured_sequence = ""
+capture_threshold = 7  # Number of frames to detect the same symbol before capturing it
+corrected_sequence = ""  # sequence of words after typo correction
+context_buffer = [] # Buffer to hold the sliding window of words for context
 
 # webcam feed
 cap = cv2.VideoCapture(0)
@@ -88,6 +118,55 @@ while cap.isOpened():
             label_text = f"{predicted_label} ({predicted_prob:.2f}%)"
             cv2.putText(frame, label_text, (x_max, y_min), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2, cv2.LINE_AA)
 
+            # checking if the current symbol is the same as the previous symbol
+            if predicted_label == current_symbol:
+                current_symbol_count += 1
+            else:
+                current_symbol = predicted_label
+                current_symbol_count = 1
+
+             # if the symbol has been detected consistently, add it to the captured sequence
+            if current_symbol_count == capture_threshold and current_symbol != previous_symbol: # avoiding adding the same symbol twice (right now only 1 unique letter captured at a time)
+                if predicted_label == 'space':
+                    # if space is detected, taking the last word from captured_sequence and uploading for correction
+                    words = captured_sequence.strip().rsplit(' ', 1)
+                    if len(words) > 0:
+                        # corrected_word = correct_typo(words[-1], context_buffer)
+                        # context_buffer.append(corrected_word)  # add the corrected word to the context buffer
+                        
+                        if len(context_buffer) > 5:  # maintaining the last 5 words for context by removing the oldest word
+                            context_buffer.pop(0)  
+                        
+                        # converting the corrected word to speech and playing it
+                        #tts = gtts.gTTS(corrected_word, slow=True)
+                        #tts.save("output.mp3")
+                        #playsound("output.mp3")
+
+                        # adding the corrected word to the corrected_sequence
+                        #corrected_sequence += corrected_word.upper() + ' '
+                        captured_sequence += ' '
+
+                # text deletion (to fix)
+                elif predicted_label == 'del' and len(captured_sequence) > 0:
+                    captured_sequence = captured_sequence[:-1]  # remove the last character for "del"
+                    # handle deletion for corrected_sequence
+                    if corrected_sequence and corrected_sequence[-1] == ' ':
+                        # remove the last word from corrected_sequence
+                        corrected_sequence = corrected_sequence.rsplit(' ', 2)[0] + ' '
+                    else:
+                        # remove the last character from corrected_sequence
+                        corrected_sequence = corrected_sequence[:-1]
+                else:
+                    captured_sequence += predicted_label  # Append the detected symbol
+                    previous_symbol = current_symbol
+                # resetting the counter after adding or deleting the symbol
+                current_symbol_count = 0
+
+    # displaying the captured sequence on the frame
+    cv2.putText(frame, f"Captured: {captured_sequence}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (147, 20, 255), 2, cv2.LINE_AA)
+    # displaying the corrected sequence on the frame
+    cv2.putText(frame, f"Corrected: {corrected_sequence}", (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2, cv2.LINE_AA)
+    
     cv2.imshow("ASL Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
