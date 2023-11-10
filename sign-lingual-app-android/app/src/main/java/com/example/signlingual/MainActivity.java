d4c0fc71345474cce34a106c49509ecc7ebd703f
Andrew Chebli <andrew.chebli@outlook.com>
2023-11-10 02:40:41 -0500

    main activity sliding menu and unversioned files

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
@@ -13,59 +13,35 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+
 import android.widget.Toast;
 
+
+import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.ktx.Firebase;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private NavigationView navigationView;
-    private FirebaseAnalytics mFirebaseAnalytics;
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         setupUI();
         loadPreferences();
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("sentence");
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // Check if there is any data
-                if (snapshot.exists()) {
-                    // Get the key from the parent of the snapshot
-                    String key = snapshot.getKey();
-
-                    // Log the key
-                    Log.d("MainActivity", "Key is: " + key);
-
-                    // If you want to get the value, you can do so like this:
-                    String value = snapshot.getValue().toString();
-                    Log.d("MainActivity", "Value is: " + value);
-                    Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
-                } else {
-                    Log.d("MainActivity", "Data does not exist at this location");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("MainActivity", "Failed to read value.", error.toException());
-            }
-        });
 
     }
 
@@ -104,18 +80,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (itemID == R.id.navHome) {
             params.putString("message", "User navigated to Home");
-            mFirebaseAnalytics.logEvent("navHome", params);
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         } else if (itemID == R.id.page1) {
             params.putString("message", "User navigated to Page 1");
-            mFirebaseAnalytics.logEvent("page1", params);
             Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
             startActivity(intent);
         } else if (itemID == R.id.settings) {
             params.putString("message", "User navigated to Settings");
-            mFirebaseAnalytics.logEvent("settings", params);
             Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent);
+        }else if (itemID == R.id.liveTranslation) {
+            params.putString("message", "User navigated to Live Translation");
+            Intent intent = new Intent(getApplicationContext(), LiveTranslation.class);
             startActivity(intent);
         }
         drawerLayout.closeDrawer(GravityCompat.START);
