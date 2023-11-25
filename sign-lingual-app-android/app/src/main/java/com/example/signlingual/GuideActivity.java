6a473370e7e9c6ff5c1d55866a3ef11f29c022cb
mycoal2 <michaelhong136@gmail.com>
2023-11-25 18:15:35 -0500

    Add sign language alphabet pictures

---
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/GuideActivity.java
@@ -0,0 +1,16 @@
+package com.example.signlingual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+
+    }
+}
