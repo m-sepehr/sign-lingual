e62df9c32d1726462310ae3f9b805c3d2db8cc04
AndrewChebli <andrew.chebli@outlook.com>
2023-11-27 22:28:40 -0500

    added shared preferences to LiveTranslation

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/LiveTranslation.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/LiveTranslation.java
@@ -1,55 +1,50 @@
 package com.example.signlingual;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
-public class LiveTranslation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class LiveTranslation extends BaseActivity {
     String message="";
     TextView conversation;
     Button launch, stop;
     FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     DatabaseReference readyReference;
+    DatabaseReference userRef, databaseReference;
+    SharedPreferences sharedPreferences;
 
-    private NavigationView navigationView;
-    private DrawerLayout drawerLayout;
     private boolean readable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_live_translation);
-        String userID = getIntent().getStringExtra("userID");
-        DatabaseReference userRef = mDatabase.getReference("users").child(userID);
-        DatabaseReference databaseReference = userRef.child("sentence");
-        readyReference = userRef.child("ready");
-        conversation = findViewById(R.id.text_translated);
-        launch = findViewById(R.id.start_translation);
-        stop = findViewById(R.id.stop_translation);
-        readyReference.setValue(false);
 
+        setContentView(R.layout.activity_live_translation);
+        sharedPreferences = getSharedPreferences("Credentials", MODE_PRIVATE);
+        userID = sharedPreferences.getString("userID", null);
+        if (userID != null) {
+            userRef = mDatabase.getReference("users").child(userID);
+            databaseReference = userRef.child("sentence");
+            readyReference = userRef.child("ready");
+            conversation = findViewById(R.id.text_translated);
+            launch = findViewById(R.id.start_translation);
+            stop = findViewById(R.id.stop_translation);
+            readyReference.setValue(false);
+        }else{
+            Log.d("LiveTranslation", "userID is null");
+            finish();
+        }
 
 
 
@@ -127,85 +122,23 @@ public class LiveTranslation extends AppCompatActivity implements NavigationView
             }
         });
     }
-
-
-
-    private void setupUI() {
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigationView);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        toolbar.setBackgroundColor(Color.BLACK);
-        toolbar.setTitle("SignLingual");
-        toolbar.setTitleTextColor(Color.WHITE);
-
-        getSupportActionBar().setTitle("SignLingual");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
+    //when leaving app, set the ready in Firebase to false
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemID = item.getItemId();
-        if (itemID == android.R.id.home) {
-            if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-//                Toast.makeText(MainActivity.this, "inside", Toast.LENGTH_SHORT).show();
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START);
-//                Toast.makeText(MainActivity.this, "else", Toast.LENGTH_SHORT).show();
-            }
+    protected void onPause() {
+        super.onPause();
+        // Set readyReference to false when the activity is paused
+        if (readyReference != null) {
+            readyReference.setValue(false);
+            launch.setClickable(true);
+            launch.setVisibility(View.VISIBLE);
+            stop.setVisibility(View.INVISIBLE);
+            stop.setClickable(false);
+
         }
-        return true;
     }
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int itemID = item.getItemId();
-        Bundle params = new Bundle();  // Create a bundle to hold parameters
 
-        if (itemID == R.id.navHome) {
-            params.putString("message", "User navigated to Home");
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            readyReference.setValue(false);
-            startActivity(intent);
-        } else if (itemID == R.id.page1) {
-            params.putString("message", "User navigated to Page 1");
-            Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            readyReference.setValue(false);
-            startActivity(intent);
-        } else if (itemID == R.id.settings) {
-            params.putString("message", "User navigated to Settings");
-            String userID = getIntent().getStringExtra("userID");
-            Intent intent = new Intent(this, SettingsActivity.class);
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            readyReference.setValue(false);
-            startActivity(intent);
-        }else if (itemID == R.id.liveTranslation) {
-            params.putString("message", "User navigated to Live Translation");
-            Intent intent = new Intent(getApplicationContext(), LiveTranslation.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            readyReference.setValue(false);
-            startActivity(intent);
-        } else if (itemID == R.id.standaloneMode) {
-            params.putString("message", "User navigated to Standalone");
-            Intent intent = new Intent(getApplicationContext(), StandaloneActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            readyReference.setValue(false);
-            startActivity(intent);
-        } else if (itemID == R.id.LogOut) {
-            params.putString("message", "User signed out");
-            Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-            startActivity(intent);
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+    protected void setupUI() {
+        super.setupUI();
     }
 }
