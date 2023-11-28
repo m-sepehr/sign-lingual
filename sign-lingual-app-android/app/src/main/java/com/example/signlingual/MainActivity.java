cc8fa8d29ef090cdececaf942d30a8daa1d4ea16
AndrewChebli <andrew.chebli@outlook.com>
2023-11-27 22:39:12 -0500

    updating MainActivity with main

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/MainActivity.java
@@ -1,41 +1,21 @@
 package com.example.signlingual;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.os.LocaleListCompat;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
-
-import android.widget.Toast;
 
 
-import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
-import android.Manifest;
-import com.chaquo.python.PyObject;
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
-
-import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity {
 
-    private NavigationView navigationView;
-    private DrawerLayout drawerLayout;
     private Button buttonSettings, buttonGuide;
     private LinearLayout layoutStandalone, layoutConRPI;
 
@@ -46,15 +26,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         setupUI();
         loadPreferences();
-
     }
 
 
 
-    private void setupUI() {
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigationView);
-        navigationView.setNavigationItemSelectedListener(this);
+    protected void setupUI() {
+        super.setupUI();
         buttonSettings = findViewById(R.id.buttonSettings);
         buttonGuide = findViewById(R.id.buttonGuide);
         layoutStandalone = findViewById(R.id.layoutStandalone);
@@ -91,74 +68,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(intent);
             }
         });
-
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setBackgroundColor(Color.BLACK);
-        getSupportActionBar().setTitle("SignLingual");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemID = item.getItemId();
-        if (itemID == android.R.id.home) {
-            if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-//                Toast.makeText(MainActivity.this, "inside", Toast.LENGTH_SHORT).show();
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START);
-//                Toast.makeText(MainActivity.this, "else", Toast.LENGTH_SHORT).show();
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int itemID = item.getItemId();
-        Bundle params = new Bundle();  // Create a bundle to hold parameters
-
-        if (itemID == R.id.navHome) {
-            params.putString("message", "User navigated to Home");
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            startActivity(intent);
-        } else if (itemID == R.id.page1) {
-            params.putString("message", "User navigated to Page 1");
-            Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            startActivity(intent);
-        } else if (itemID == R.id.settings) {
-            params.putString("message", "User navigated to Settings");
-                String userID = getIntent().getStringExtra("userID");
-                Intent intent = new Intent(this, SettingsActivity.class);
-                intent.putExtra("userID", userID); // Pass the userID as an extra
-                startActivity(intent);
-        }else if (itemID == R.id.liveTranslation) {
-            params.putString("message", "User navigated to Live Translation");
-            Intent intent = new Intent(getApplicationContext(), LiveTranslation.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            startActivity(intent);
-        } else if (itemID == R.id.standaloneMode) {
-            params.putString("message", "User navigated to Standalone");
-            Intent intent = new Intent(getApplicationContext(), StandaloneActivity.class);
-            String userID = getIntent().getStringExtra("userID");
-            intent.putExtra("userID", userID); // Pass the userID as an extra
-            startActivity(intent);
-        } else if (itemID == R.id.LogOut) {
-            params.putString("message", "User signed out");
-            Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-            startActivity(intent);
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
     private void loadPreferences() {
         // Set Language Configs. Check Android version for implementing backwards compatibility
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
