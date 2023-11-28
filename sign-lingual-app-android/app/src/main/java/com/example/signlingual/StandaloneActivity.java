5cada55d9b4a88259070b897a4340934c8f37f84
mycoal2 <michaelhong136@gmail.com>
2023-11-28 00:54:52 -0500

    moved cameraActivity into StandaloneActivity and centered login page

--- sign-lingual-app-android/app/src/main/java/com/example/signlingual/StandaloneActivity.java
+++ sign-lingual-app-android/app/src/main/java/com/example/signlingual/StandaloneActivity.java
@@ -1,14 +1,100 @@
 package com.example.signlingual;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.ImageReader;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 
-public class StandaloneActivity extends BaseActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+
+public class StandaloneActivity extends BaseActivity implements ImageReader.OnImageAvailableListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standalone);
 
+        askCameraPermission();
+
         setupUI();
+    }
+    public void askCameraPermission() {
+        //TODO ask for camera permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.CAMERA}, 121);
+            }else{
+                setFragment();
+            }
+        } else {
+            setFragment();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setFragment();
+        } else {
+            finish();
+        }
+    }
+    int previewHeight = 0,previewWidth = 0;
+    int sensorOrientation;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected void setFragment() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+        try {
+            cameraId = manager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        CameraConnectionFragment fragment;
+        CameraConnectionFragment camera2Fragment =
+                CameraConnectionFragment.newInstance(
+                        new CameraConnectionFragment.ConnectionCallback() {
+                            @Override
+                            public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                previewHeight = size.getHeight();
+                                previewWidth = size.getWidth();
+                                Log.d("tryOrientation","rotation: "+rotation+"   orientation: "+getScreenOrientation()+"  "+previewWidth+"   "+previewHeight);
+                                sensorOrientation = rotation - getScreenOrientation();
+                            }
+                        },
+                        this,
+                        R.layout.camera_fragment,
+                        new Size(640, 480));
+
+        camera2Fragment.setCamera(cameraId);
+        fragment = camera2Fragment;
+        getFragmentManager().beginTransaction().replace(R.id.container2, fragment).commit();
+    }
+    protected int getScreenOrientation() {
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_270:
+                return 270;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_90:
+                return 90;
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public void onImageAvailable(ImageReader imageReader) {
+        imageReader.acquireLatestImage().close();
     }
 
     protected void setupUI() {
