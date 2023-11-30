db40234e1d73d5b93346f79ca1bd9592cbe4064a
m-sepehr <m-sepehr@hotmail.com>
2023-11-30 18:19:48 -0500

    adding ability for server to request ready status from firebase in order to launch or start a script.
    Co-authored-by: AndrewChebli <andrew.chebli@outlook.com>

--- sign-lingual-pc-host/main/server.py
+++ sign-lingual-pc-host/main/server.py
@@ -2,36 +2,61 @@ import threading
 from flask import Flask, request
 from zeroconf import Zeroconf, ServiceInfo
 import socket
-
-# converting the IP address to its byte representation
-address_byte = socket.inet_aton("127.0.0.1")
-
-service_type = "_http._tcp.local."
-service_name = "MyService"
-service_port = 12345
-
-zeroconf = Zeroconf()
-full_service_name = f"{service_name}.{service_type}"  # complete service name including the type
-info = ServiceInfo(service_type, full_service_name, addresses=[address_byte], port=service_port, weight=0, priority=0)
-zeroconf.register_service(info)
+import requests
+import time
 
 app = Flask(__name__)
 
+# Global variables
+running = False
+database_url = ''
+user_token = ''
+uid = ''
+
+def get_ready_status():
+    url = f"{database_url}/ready.json?auth=" + user_token
+    response = requests.get(url)
+    ready_status = response.json()
+    print("Ready status response:", ready_status, "Type:", type(ready_status))
+    return ready_status
+
 @app.route('/receiveUserData', methods=['POST'])
 def receive_user_data():
+    global uid, user_token, database_url
     uid = request.form.get('uid')
-    token = request.form.get('token')
-
-    print(f"Received UID: {uid}, Token: {token}")
-
-    # This is where the actions are to be performed
-
+    user_token = request.form.get('token')
+    database_url = "https://signlingual-901cc-default-rtdb.firebaseio.com/users/" + uid
+    print(f"Received UID: {uid}, Token: {user_token}")
     return "Data received successfully"
 
+def check_status_loop():
+    global running
+    while True:
+        if uid and user_token and database_url:
+            ready = get_ready_status()
+            if ready and not running:
+                print(".....Launching......")
+                running = True
+            elif not ready and running:
+                print(".....Stopping......")
+                running = False
+        time.sleep(1)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=service_port)
+    service_type = "_http._tcp.local."
+    service_name = "MyService"
+    service_port = 12345
+    address_byte = socket.inet_aton("127.0.0.1")
+    zeroconf = Zeroconf()
+    full_service_name = f"{service_name}.{service_type}"
+    info = ServiceInfo(service_type, full_service_name, addresses=[address_byte], port=service_port, weight=0, priority=0)
+    zeroconf.register_service(info)
 
-zeroconf.unregister_service(info)
-zeroconf.close()
+    status_thread = threading.Thread(target=check_status_loop)
+    status_thread.start()
 
-       
+    try:
+        app.run(host='0.0.0.0', port=service_port)
+    finally:
+        zeroconf.unregister_service(info)
+        zeroconf.close()
