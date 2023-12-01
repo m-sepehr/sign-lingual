7498cf68b2e1d478dc9cdd79ca25ff6b40be1b27
m-sepehr <m-sepehr@hotmail.com>
2023-12-01 04:19:21 -0500

    added ability to reset configuration while server is running. added keyboard listener to detect reset input. adding config file to store uid. added function to check network connection. added ability to retrieve local ip and send it to firebase. added exception handling to firebase authentication to prevent crashing.

--- sign-lingual-pc-host/main/server.py
+++ sign-lingual-pc-host/main/server.py
@@ -9,6 +9,11 @@ import os
 import subprocess
 import cv2
 from pyzbar.pyzbar import decode
+import socket
+import json
+from pynput import keyboard
+import pdb
+from requests.exceptions import ConnectionError, Timeout
 
 app = Flask(__name__)
 
@@ -17,6 +22,54 @@ running = False
 database_url = ''
 user_token = ''
 uid = ''
+
+
+# ----------------- Keyboard Input for Reset -----------------
+def on_press(key):
+    global uid, user_token, database_url
+    try:
+        if key.char == 'q':
+            print('q was pressed')
+            print("Resetting configuration...")
+            # Reset the UID, user token, and database URL
+            uid, user_token, database_url = None, None, None
+            # Delete the configuration file
+            os.remove('config.json')
+
+            # Trigger QR code scanning and reconfiguration here
+            qr_code_data = scan_qr_code()
+            configure_wifi(qr_code_data)
+
+            # Send the local IP address to Firebase
+            local_ip = get_ip_address()
+            send_data(local_ip)
+
+    except AttributeError:
+        pass
+# --------------------------------------------------
+
+# ----------------- Keyboard Listener -----------------
+def start_keyboard_listener():
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+# -----------------------------------------------------
+
+# ----------------- Saving configuration -----------------
+# Function to save configuration data
+def save_config(data, filename='config.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+# ---------------------------------------------------------
+
+# ----------------- Loading configuration -----------------
+# Function to load configuration data
+def load_config(filename='config.json'):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+# ---------------------------------------------------------
 
 # ----------------- QR Code Scanning -----------------
 # Function to scan QR code
@@ -36,7 +89,7 @@ def scan_qr_code():
                 return obj.data.decode("utf-8")
 
 
-        cv2.imshow("QR Code Scanner", frame)
+        #cv2.imshow("QR Code Scanner", frame)
 
         # Break the loop if 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -45,30 +98,99 @@ def scan_qr_code():
     cap.release()
     cv2.destroyAllWindows()
 
-# Function to configure Wi-Fi based on QR code data
+# function to configure Wi-Fi based on QR code data
 def configure_wifi(qr_code_data):
+    global uid, user_token, database_url, network_online
     # Extract Wi-Fi information from QR code
     wifi_info = qr_code_data.split(';')
     first_term = wifi_info[0].split(':')[-1]
     ssid = first_term.split('|')[0]
-    userID =first_term.split('|')[-2]
-    token =first_term.split('|')[-1]
+    uid =first_term.split('|')[-2]
+    database_url = "https://signlingual-901cc-default-rtdb.firebaseio.com/users/" + uid
+    user_token =first_term.split('|')[-1]
     password = wifi_info[2].split(':')[-1]
+
+    config_data = {
+        'uid' : uid
+    }
+
+    save_config(config_data)  # saving the configuration data
 
     # Configure Wi-Fi using subprocess (for Windows)
     subprocess.run(['networksetup', '-setairportnetwork', 'en0', ssid, password])
+    
+    while (True):
+        if is_connected_to_internet():
+            network_online = True
+            break
+        else:
+            network_online = False
+        time.sleep(1)
 
-# ----------------- QR Code Scanning -----------------
+# ------------------------------------------------------
 
+# ----------------- Checking network connection -----------------
+def is_connected_to_internet(url='http://www.google.com/', timeout=5):
+    try:
+        # Attempt to make a request to the specified URL
+        requests.get(url, timeout=timeout)
+        print("Connected to internet.")
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+    return False
+# ---------------------------------------------------------------
+
+# ----------------- Retrieving local IP address -----------------
+def get_ip_address():
+    # Attempt to connect to an external host to determine the local IP
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Google's DNS
+        ip_address = s.getsockname()[0]
+        s.close()
+    except Exception as e:
+        print(f"Error getting IP Address: {e}")
+        ip_address = None
+    return ip_address
+# ---------------------------------------------------------------
+
+# ----------------- Sending local IP to Firebase -----------------
+def send_data(local_ip):
+    global user_token, database_url
+    response = requests.put(f"{database_url}/ip_address.json?auth=" +  user_token, json=local_ip)
+    print("Sending data response:", response.status_code, response.json())  # Debugging print
+    if response.status_code == 200:
+        print("Data sent to Firebase.")
+    else:
+        print(f"Failed to send data. Status code: {response.status_code}")
+# ---------------------------------------------------------------
 
 # ----------------- Firebase Authentication -----------------
 def get_ready_status():
     url = f"{database_url}/ready.json?auth=" + user_token
-    response = requests.get(url)
-    ready_status = response.json()
-    print("Ready status response:", ready_status, "Type:", type(ready_status))
-    return ready_status
-# ----------------- Firebase Authentication -----------------
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            ready_status = response.json()
+            print("Ready status response:", ready_status, "Type:", type(ready_status))
+            return ready_status
+        else:
+            print(f"Error: Received non-200 response {response.status_code}")
+            return None
+    except ConnectionError:
+        print("Error: Failed to connect to the Firebase URL.")
+        # Handle the connection error (e.g., retry, log, or take alternative actions)
+        return None
+    except Timeout:
+        print("Error: Request to Firebase timed out.")
+        # Handle timeout error
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        # Handle other potential exceptions
+        return None
+# ---------------------------------------------------------------
 
 @app.route('/receiveUserData', methods=['POST'])
 def receive_user_data():
@@ -84,11 +206,12 @@ def receive_user_data():
     return "Data received successfully"
 
 def check_status_loop():
-    global running
+    global running, network_online
     script_process = None
 
     while True:
-        if uid and user_token and database_url:
+        print (network_online)
+        if uid and user_token and database_url and network_online:
             ready = get_ready_status()
             if ready and not running:
                 print(".....Launching......")
@@ -109,10 +232,29 @@ def check_status_loop():
                 running = False
         time.sleep(1)
 
+network_online = is_connected_to_internet()
+
 if __name__ == '__main__':
-    # Scan the QR code and configure Wi-Fi
-    qr_code_data = scan_qr_code()
-    configure_wifi(qr_code_data)
+
+    # Try to load existing configuration
+    config = load_config()
+
+    if config:
+        uid = config['uid']
+        database_url = "https://signlingual-901cc-default-rtdb.firebaseio.com/users/" + uid
+    else:
+        # Configuration does not exist, scan QR code and configure Wi-Fi
+        qr_code_data = scan_qr_code()
+        configure_wifi(qr_code_data)
+
+    # Start the keyboard listener thread
+    keyboard_thread = threading.Thread(target=start_keyboard_listener)
+    keyboard_thread.start()
+    
+    local_ip = get_ip_address()
+
+    if (user_token != ''):
+        send_data(local_ip)
 
     # Starting the server
     service_type = "_http._tcp.local."
