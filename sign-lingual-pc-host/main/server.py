3632509a8775be59e0a066dad79b463faa2f958f
m-sepehr <m-sepehr@hotmail.com>
2023-11-30 23:59:10 -0500

    adding QR code detection functionality for hardware configuration

--- sign-lingual-pc-host/main/server.py
+++ sign-lingual-pc-host/main/server.py
@@ -7,6 +7,8 @@ import time
 import sys
 import os
 import subprocess
+import cv2
+from pyzbar.pyzbar import decode
 
 app = Flask(__name__)
 
@@ -16,12 +18,57 @@ database_url = ''
 user_token = ''
 uid = ''
 
+# ----------------- QR Code Scanning -----------------
+# Function to scan QR code
+def scan_qr_code():
+    # Open a video capture device (you may need to change the index based on your setup)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        # Decode QR code
+        decoded_objects = decode(frame)
+
+        for obj in decoded_objects:
+            # Assuming the QR code contains Wi-Fi credentials in the format "WIFI:S:SSID;T:WPA;P:password;;"
+            if obj.type == 'QRCODE' and b'WIFI:' in obj.data.upper():
+                return obj.data.decode("utf-8")
+
+
+        cv2.imshow("QR Code Scanner", frame)
+
+        # Break the loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Function to configure Wi-Fi based on QR code data
+def configure_wifi(qr_code_data):
+    # Extract Wi-Fi information from QR code
+    wifi_info = qr_code_data.split(';')
+    first_term = wifi_info[0].split(':')[-1]
+    ssid = first_term.split('|')[0]
+    userID =first_term.split('|')[-2]
+    token =first_term.split('|')[-1]
+    password = wifi_info[2].split(':')[-1]
+
+    # Configure Wi-Fi using subprocess (for Windows)
+    subprocess.run(['networksetup', '-setairportnetwork', 'en0', ssid, password])
+
+# ----------------- QR Code Scanning -----------------
+
+
+# ----------------- Firebase Authentication -----------------
 def get_ready_status():
     url = f"{database_url}/ready.json?auth=" + user_token
     response = requests.get(url)
     ready_status = response.json()
     print("Ready status response:", ready_status, "Type:", type(ready_status))
     return ready_status
+# ----------------- Firebase Authentication -----------------
 
 @app.route('/receiveUserData', methods=['POST'])
 def receive_user_data():
@@ -63,6 +110,11 @@ def check_status_loop():
         time.sleep(1)
 
 if __name__ == '__main__':
+    # Scan the QR code and configure Wi-Fi
+    qr_code_data = scan_qr_code()
+    configure_wifi(qr_code_data)
+
+    # Starting the server
     service_type = "_http._tcp.local."
     service_name = "MyService"
     service_port = 12345
